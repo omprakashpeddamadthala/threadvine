bfddd524fe379bad96758b69c4b91f96adcd1e14
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-04-08 11:50:37 +0530

    project basic setup

---
+++ src/main/java/com/threadvine/ThreadvineApplication.java
@@ -0,0 +1,13 @@
+package com.threadvine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadvineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( ThreadvineApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/threadvine/ThreadvineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threadvine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadvineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
