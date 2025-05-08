57663f05e2e86dd7da08b609c828c9a23c54e845
Om Prakash <omprakashpeddamadthala@hotmail.com>
2025-05-08 11:01:28 +0530

    implemented order controller create order api

--- src/main/java/com/threadvine/service/OrderService.java
+++ src/main/java/com/threadvine/service/OrderService.java
@@ -1,4 +1,10 @@
 package com.threadvine.service;
 
+import com.threadvine.dto.OrderDTO;
+
+import java.util.UUID;
+
 public interface OrderService {
+
+    OrderDTO createOrder(UUID userId,String address ,String phoneNumber);
 }
